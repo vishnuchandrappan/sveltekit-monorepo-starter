7e5ddf7f85a0ebc7431e578d9dd7b9163269e524
Vish <32509220+vishnuchandrappan@users.noreply.github.com>
2024-09-15 16:54:17 +0530

    feat: `unplugin-icons` + `@iconify/json`

--- README.md
+++ README.md
@@ -34,3 +34,7 @@ npm run build
 You can preview the production build with `npm run preview`.
 
 > To deploy your app, you may need to install an [adapter](https://kit.svelte.dev/docs/adapters) for your target environment.
+
+## Icons
+
+[Icones](https://icones.js.org/)
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,32 @@
+<script lang="ts">
+	import EsLintLogo from 'virtual:icons/logos/eslint';
+	import PrettierLogo from 'virtual:icons/logos/prettier';
+	import SvelteLogo from 'virtual:icons/logos/svelte-icon';
+	import TailwindLogo from 'virtual:icons/logos/tailwindcss-icon';
+	import TypeScriptLogo from 'virtual:icons/logos/typescript-icon';
+</script>
+
 <div
-	class="flex h-screen w-screen items-center justify-center bg-gradient-to-r from-indigo-500 to-blue-950"
+	class="flex h-screen w-screen flex-col items-center justify-center gap-4 bg-gradient-to-r from-indigo-500 to-blue-950"
 >
 	<h1 class="text-6xl font-bold text-white">Hello, World!</h1>
+	<div class="flex flex-col gap-2 text-white">
+		<div class="e flex flex-wrap items-center gap-2 rounded-xl border-2 p-4 text-4xl">
+			<SvelteLogo />
+			<span>+</span>
+			<TypeScriptLogo />
+			<span>+</span>
+			<TailwindLogo />
+			<span>+</span>
+			<PrettierLogo />
+			<span>+</span>
+			<EsLintLogo />
+		</div>
+		<div class="self-center rounded-xl border-2 p-4">
+			<ul class="list-inside list-disc">
+				<li>lint-staged + husky</li>
+				<li>unplugin icons</li>
+			</ul>
+		</div>
+	</div>
 </div>
